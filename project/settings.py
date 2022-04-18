62b3011f4eefbb94267891045d8828b6121449c4
muhammadgalhoum <muhammadgalhoum@gmail.com>
2022-04-18 17:42:36 +0200

    Updating settings.py file

--- project/settings.py
+++ project/settings.py
@@ -149,5 +149,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'muhammadgalhoum@gmail.com' #os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = 'Google&Microsoft_86'#os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER = '' # put your email here
+EMAIL_HOST_PASSWORD = '' # put your email password here
